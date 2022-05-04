f5d14e23992a7e0f67ce023f9041f98793f3e89d
SamarV-121 <samarvispute121@pm.me>
2022-05-04 18:46:55 +0530

    RM6785: Import powerscntbl.xml
    Change-Id: Ie08918d62ab441c929f162653ac1b85036956152
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- device.mk
+++ device.mk
@@ -243,6 +243,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-V1-ndk_platform.vendor \
     android.hardware.power@1.3.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/power/powerscntbl.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerscntbl.xml
+
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
