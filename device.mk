e13e95456b4c999803bd0227131c150c296d1c70
officialputuid <officialputuid@hack.id>
2022-05-07 15:51:54 +0800

    RM6785: Added apns-conf from stock/RMX2001_C18
    Change-Id: I44a55990e6c5ba93b2ca57cd8c369311812fc616

--- device.mk
+++ device.mk
@@ -60,6 +60,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# APN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor
